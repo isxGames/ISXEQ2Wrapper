3fcb38ad16ea2cac7d5f9dd1a3f81667919f0413
eccentric <eccentric>
2009-11-02 10:37:32 +0000

    Corrected the Item property to be indexed by integer or substring as a pair of overloaded functions.

--- LootWindow.cs
+++ LootWindow.cs
@@ -10,96 +10,99 @@ using LavishScriptAPI;
 
 namespace EQ2.ISXEQ2
 {
-    public class LootWindow : LavishScriptObject
-    {
-        public LootWindow(LavishScriptObject Obj)
-            : base(Obj)
-        {
-        }
+	public class LootWindow : LavishScriptObject
+	{
+		public LootWindow(LavishScriptObject Obj)
+			: base(Obj)
+		{
+		}
 
-        public int NumItems
-        {
-            get
-            {
-                return GetMember<int>("NumItems");
-            }
-        }
+		public int NumItems
+		{
+			get
+			{
+				return GetMember<int>("NumItems");
+			}
+		}
 
-        public Item Item
-        {
-            get
-            {
-                LavishScriptObject Obj = GetMember("Item");
-                return new Item(Obj);
-            }
-        }
+		public EQ2UIPage ToEQ2UIPage
+		{
+			get
+			{
+				LavishScriptObject Obj = GetMember("ToEQ2UIPage");
+				return new EQ2UIPage(Obj);
+			}
+		}
 
-        public EQ2UIPage ToEQ2UIPage
-        {
-            get
-            {
-                LavishScriptObject Obj = GetMember("ToEQ2UIPage");
-                return new EQ2UIPage(Obj);
-            }
-        }
+		public bool IsLotto
+		{
+			get
+			{
+				return GetMember<bool>("IsLotto");
+			}
+		}
 
-        public bool IsLotto
-        {
-            get
-            {
-                return GetMember<bool>("IsLotto");
-            }
-        }
+		public string Type
+		{
+			get
+			{
+				return GetMember<string>("Type");
+			}
+		}
 
-        public string Type
-        {
-            get
-            {
-                return GetMember<string>("Type");
-            }
-        }
+		public Item Item(int ItemIndex)
+		{
+			LavishScriptObject Obj = GetMember("Item", ItemIndex.ToString());
+			return new Item(Obj);
+		}
 
-        public bool LootAll()
-        {
-            return ExecuteMethod("LootAll");
-        }
+		public Item Item(string Label)
+		{
+			LavishScriptObject Obj = GetMember("Item", Label);
+			return new Item(Obj);
+		}
 
-        public bool RequestAll()
-        {
-            return ExecuteMethod("RequestAll");
-        }
+		public bool LootAll()
+		{
+			return ExecuteMethod("LootAll");
+		}
 
-        public bool DeclineLotto()
-        {
-            return ExecuteMethod("DeclineLotto");
-        }
+		public bool RequestAll()
+		{
+			return ExecuteMethod("RequestAll");
+		}
 
-        public bool LootItem()
-        {
-            return ExecuteMethod("LootItem");
-        }
+		public bool DeclineLotto()
+		{
+			return ExecuteMethod("DeclineLotto");
+		}
 
-        public bool LootItem(int ID, bool LootNoTrade)
-        {
-            if (LootNoTrade)
-                return ExecuteMethod("LootItem", ID.ToString());
-            return ExecuteMethod("LootItem", ID.ToString(), "0");
-        }
+		public bool LootItem()
+		{
+			return ExecuteMethod("LootItem");
+		}
 
-        public bool SelectNeed()
-        {
-            return ExecuteMethod("SelectNeed");
-        }
+		public bool LootItem(int ID, bool LootNoTrade)
+		{
+			if (LootNoTrade)
+				return ExecuteMethod("LootItem", ID.ToString());
+			return ExecuteMethod("LootItem", ID.ToString(), "0");
+		}
 
-        public bool SelectGreed()
-        {
-            return ExecuteMethod("SelectGreed");
-        }
+		public bool SelectNeed()
+		{
+			return ExecuteMethod("SelectNeed");
+		}
 
-        public bool DeclineNBG()
-        {
-            return ExecuteMethod("DeclineNBG");
-        }
+		public bool SelectGreed()
+		{
+			return ExecuteMethod("SelectGreed");
+		}
 
-    }
+		public bool DeclineNBG()
+		{
+			return ExecuteMethod("DeclineNBG");
+		}
+
+	}
 }
