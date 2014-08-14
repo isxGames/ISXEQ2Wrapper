0e3a0b874a92f2580b85597764d666debe15589c
Raymond Lane <raymond_lane@msn.com>
2014-08-14 15:46:18 -0500

    Added Custom TraceListener

---
+++ ISXEQ2WrapperTest/MyTraceListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISXEQ2WrapperTest
+{
+    public class MyTraceListener : TraceListener
+    {
+        public MyTraceListener(MainWindow window)
+        {
+            this._window = window;
+            //if (!Trace.Listeners.Contains(this))
+            //    Trace.Listeners.Add(this);
+        }
+
+        private MainWindow _window;
+
+        public override void Write(string message)
+        {
+            _window.Log(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            _window.Log(message);
+        }
+    }
+}
